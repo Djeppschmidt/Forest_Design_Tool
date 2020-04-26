145033c3d395d6e75298586ee3d0f1fbad50dc3c
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2020-04-26 00:12:40 -0400

    Create ForestDesignFunctions.R
    List of functions for the tool

---
+++ ForestDesignFunctions.R
@@ -0,0 +1,62 @@
+
+Test.df<-data.frame("Names"=,""...) # multiple data frames
+
+# df(niche characteristics), df(common associates), habit, 
+
+# 'b' %in% v where b is the searched item and v is the vector
+
+
+#' list all plants that are in canopy and could possibly be used in the canopy with selected species
+#' @param reps number of replicate communities
+#' @keywords benchmark
+#' @export
+#' @examples test.script<-benchmark.MM(reps=3, commonN=20, groupN=10, singleN=10, D=2000, V=1000)
+#'benchmark.MM()
+
+print.canopy<-function(pH, OM, Climate,){print(Plants$Names[1,Plants$|...])}
+
+#' list all plants that are in canopy and could possibly be used in the understory with selected species
+#' @param reps number of replicate communities
+#' @keywords benchmark
+#' @export
+#' @examples test.script<-benchmark.MM(reps=3, commonN=20, groupN=10, singleN=10, D=2000, V=1000)
+#'benchmark.MM()
+
+print.understory<-function(){print(Plants$Names[1,Plants$|...])}
+
+#' list all plants that are in canopy and could possibly be used in the ground level with selected species
+#' @param reps number of replicate communities
+#' @keywords benchmark
+#' @export
+#' @examples test.script<-benchmark.MM(reps=3, commonN=20, groupN=10, singleN=10, D=2000, V=1000)
+#'benchmark.MM()
+
+print.ground<-function(){print(Plants$Names[1,Plants$|...])}
+
+#' list all products from plant
+#' @param reps number of replicate communities
+#' @keywords benchmark
+#' @export
+#' @examples test.script<-benchmark.MM(reps=3, commonN=20, groupN=10, singleN=10, D=2000, V=1000)
+#'benchmark.MM()
+
+print.products<-function(){print(Plants$Names[1,Plants$|...])}
+
+#' print the propagation and growing notes
+#' @param reps number of replicate communities
+#' @keywords benchmark
+#' @export
+#' @examples test.script<-benchmark.MM(reps=3, commonN=20, groupN=10, singleN=10, D=2000, V=1000)
+#'benchmark.MM()
+
+print.notes<-function(){print(Plants$Names[1,Plants$|...])}
+
+#' Plot Icon
+#' @param reps number of replicate communities
+#' @keywords benchmark
+#' @export
+#' @examples test.script<-benchmark.MM(reps=3, commonN=20, groupN=10, singleN=10, D=2000, V=1000)
+#'benchmark.MM()
+
+Plot.Icon<-function(){print(Plants$Names[1,Plants$|...])}
+
